70d9610cf95e9da0aa70108a351b39a79f3ef609
gitgroch <geoff.rochford@gmail.com>
2022-11-30 15:02:43 +0000

    Add more code for add event function

--- eventboard/forms.py
+++ eventboard/forms.py
@@ -1,5 +1,6 @@
 from .models import Comment, Post
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -11,4 +12,21 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('content',)
+        fields = ('title', 'author', 'event_location', 'event_date_and_time',
+                    'content', 'featured_image', 'created_on', )
+
+        widgets = {
+            'overview': SummernoteWidget(),
+            'specifications': SummernoteWidget(),
+            'plans': SummernoteWidget(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields[
+                'event_image'
+                ].label = "Upload an image for your event"
+        self.fields[
+                'overview'
+                ].label = "Event Overview"
+      
