d9288ddf505eccda165384fc589f3ede7adf0891
gitgroch <geoff.rochford@gmail.com>
2022-11-30 10:36:48 +0000

    Removed votable model to start again

--- eventboard/models.py
+++ eventboard/models.py
@@ -46,3 +46,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
--- eventboard/views.py
+++ eventboard/views.py
@@ -75,3 +75,4 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        
