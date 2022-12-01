cdc6098d2a6026d40d60cafe6c294a6b4af63cf2
gitgroch <geoff.rochford@gmail.com>
2022-12-01 16:31:26 +0000

    Add request.FILES to post_new view to handle image

--- eventboard/views.py
+++ eventboard/views.py
@@ -91,7 +91,7 @@ class PostLike(View):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
